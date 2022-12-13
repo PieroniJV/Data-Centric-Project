898dbe1d1735c74a0d0f827edeeb89fd0c0f3314
Jose Vitor Pieroni <vitorpieroni1@gmail.com>
2022-12-13 17:16:11 +0000

    continue on edit.ejs/index.js

--- index.js
+++ index.js
@@ -2,28 +2,59 @@ const express = require('express');
 const app = express();
 const port = 3004;
 const ejs = require('ejs');
-app.set('view engine', 'ejs')
+const pmysql = require('promise-mysql')
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=>{
-    res.send('<h1>This is the main page</h1>');
+var pool;
+
+pmysql.createPool({
+    connectionLimit: 3,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'proj2022'
 })
-app.get('/', (req, res)=>{
-
+.then(p => {
+    pool = p
 })
-app.get('/', (req, res)=>{
-
+.catch(e => {
+    console.log("pool error:" + e)
 })
-app.get('/', (req, res)=>{
 
+app.get('/', (req, res) => {
+    res.render('home')
+    //res.send('<h1>This is the main page</h1>');
 })
-app.get('/', (req, res)=>{
-
+app.get('/employees', (req, res) => {
+    //res.render('employees')
+    pool.query('SELECT * FROM employee')
+    .then((data) => {
+        res.render('employees', {"employees": data})
+    })
+    .catch(error => {
+        reject(error)
+    })
 })
-app.get('/', (req, res)=>{
-
+app.get('/employees/edit/:eid', (req, res) => {
+    res.render('edit')
 })
-app.get('/', (req, res)=>{
-
+app.post('/employees/edit/:eid', (req, res) => {
+    console.log('test')
+})
+app.get('/depts', (req, res) => {
+    res.render('depts')
+})
+app.get('/depts/delete/:did', (req, res) => {
+    res.render('delete')
+})
+app.get('/employeesMongoDB', (req, res) => {
+    res.render('employeesMongoDB')
+})
+app.get('/employeesMongoDB/add', (req, res) => {
+    res.render('add')
+})
+app.post('/employeesMongoDB/add', (req, res) => {
+    console.log('test')
 })
 
 app.listen(port, () => {
