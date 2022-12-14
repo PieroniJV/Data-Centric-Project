aad2b39127d48df6cbe7950ba50c50ca3c6dbb87
Jose Vitor Pieroni <vitorpieroni1@gmail.com>
2022-12-14 13:36:58 +0000

    changes on edit ejs and index js

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ const app = express();
 const port = 3004;
 const ejs = require('ejs');
 const pmysql = require('promise-mysql')
+const bodyParser = require('body-parser')
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }))
 
 var pool;
+var employeeList = [];
+var role1 = "manager";
+var role2 = "employee";
 
 pmysql.createPool({
-    connectionLimit: 3,
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'proj2022'
 })
-.then(p => {
-    pool = p
-})
-.catch(e => {
-    console.log("pool error:" + e)
-})
+    .then(p => {
+        pool = p
+    })
+    .catch(e => {
+        console.log("pool error:" + e)
+    })
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -28,18 +32,51 @@ app.get('/', (req, res) => {
 app.get('/employees', (req, res) => {
     //res.render('employees')
     pool.query('SELECT * FROM employee')
-    .then((data) => {
-        res.render('employees', {"employees": data})
-    })
-    .catch(error => {
-        reject(error)
-    })
+        .then((data) => {
+            employeeList = data
+            res.render('employees', { employees: data })
+        })
+        .catch(error => {
+            reject(error)
+        });
 })
 app.get('/employees/edit/:eid', (req, res) => {
-    res.render('edit')
+    var employee = employeeList.find((employee) => {
+        if (employee.eid == req.params.eid) {
+            return employee
+        }
+    })
+
+    if (employee != undefined) {
+        res.render('edit', {
+            empId: employee.eid,
+            empSalary: employee.salary,
+            empName: employee.ename,
+            empRole: employee.role
+        })
+    }
+    else {
+        res.send("Error found.")
+    }
+
 })
 app.post('/employees/edit/:eid', (req, res) => {
-    console.log('test')
+    //var role = req.body.role;
+    var employee = employeeList.find((employee) => {
+        if (employee.eid == req.params.eid) {
+            return employee
+        } else {
+            
+        }
+    })
+
+    if (req.body.ename.length < 4) {
+        
+    } 
+    else if(req.body.role.toLowerCase() == role1 || req.body.role.toLowerCase() == role2)
+    {
+        
+    }
 })
 app.get('/depts', (req, res) => {
     res.render('depts')
